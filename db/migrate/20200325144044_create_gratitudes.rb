c4441b61b999e5a663a9555d9dea590cf05beceb
93geraldtee <93geraldtee@gmail.com>
2020-03-25 15:01:15 -0400

    add gratitudes migration and fix belongs_to

---
+++ db/migrate/20200325144044_create_gratitudes.rb
@@ -0,0 +1,11 @@
+class CreateGratitudes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gratitudes do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
