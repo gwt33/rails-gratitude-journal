642402b938539e6a747037520d3282b6b15ee5c1
93geraldtee <93geraldtee@gmail.com>
2020-03-25 15:02:54 -0400

    add comments join table and associations with resource g

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :gratitudes
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  
+  resources :comments
+  
   # required for devise
   root to: 'static#home'
 
