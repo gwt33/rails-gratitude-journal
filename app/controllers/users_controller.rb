aae8ec2e78cbe8d10798552f8fc9c539de6f0b1e
93geraldtee <93geraldtee@gmail.com>
2020-03-25 19:40:20 -0400

    add before_action and user_signed_in method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+    #through devise - ***if a user is not logged in, can't see another user's show page and redirects to the login page***
+    before_action :authenticate_user!
+
 
     #for User show page
     def show
+        user_signed_in? #devise helper method
                   # .find will break and throw an error.. would need a rescue statement
         @user = User.find_by_id(params[:id])
         redirect_to '/' if !@user
