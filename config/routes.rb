e5ba3515049adc6bdc424a305d428dc78d61a96c
93geraldtee <93geraldtee@gmail.com>
2020-03-26 10:35:38 -0400

    fix order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   root to: 'static#home'
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  resources :gratitudes do
+    resources :comments
+  end
+  
   resources :comments
 
   resources :users do
-    resources :posts, only: [:new, :create, :index]
-  end
-  
-  resources :gratitudes do
-    resources :comments, only: [:new, :create, :index]
+    resources :gratitudes, only: [:new, :create, :index] #shallow routes: only nest the routes you need (crucial to know the post_id )
   end
   
 end
