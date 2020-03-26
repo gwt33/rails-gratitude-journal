7b4d9f166144b63e820ffe37ee0f13dfeec2d200
93geraldtee <93geraldtee@gmail.com>
2020-03-26 10:36:27 -0400

    add index, new, create and strong params for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,45 @@
 class CommentsController < ApplicationController
+
+    def index
+        #need to check if it's nested
+        if params[:gratitude_id] && @gratitude = Gratitude.find_by_id(params[:gratitude_id])
+            @comments = @comment.gratitudes
+        else
+            @error = "That gratitude doesn't exist!" if params[:gratitude_id]
+            @comments = comment.all
+        end
+    end
+
+    def new
+        # 
+        if params[:gratitude_id] && @gratitude = Gratitude.find_by_id(params[:gratitude_id])
+            @comments = @gratitude.comments.build
+        else
+            @error = "That gratitude doesn't exist!" if params[:gratitude_id]
+            @comment = Comment.new
+        end
+    end
+
+    def create
+        #build doesn't save to database, but returns a new object of the associated type to be used in the view 
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            flash[:message] = "Comment created!"
+            redirect_to comments_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        # @comment = Comment.find_by_id(params[:id])
+        # redirect_to comments_path if !@comment
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :gratitude_id)
+    end
 end
