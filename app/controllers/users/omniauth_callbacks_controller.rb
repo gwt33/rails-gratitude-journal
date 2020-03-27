b0376e3230267529870a825971ac03fa775e2024
93geraldtee <93geraldtee@gmail.com>
2020-03-27 18:28:35 -0400

    move user show method to this controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :authenticate_user!
     def google_oauth2
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         @user = User.from_omniauth(request.env['omniauth.auth'])
@@ -10,6 +11,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
           redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
         end
+    end
+
+        #through devise - ***if a user is not logged in, can't see another user's show page and redirects to the login page***
+
+
+    #for User show page
+    def show
+        user_signed_in? #devise helper method
+                  # .find will break and throw an error.. would need a rescue statement
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
     end
 
 end
