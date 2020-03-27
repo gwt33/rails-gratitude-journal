50a767515f1ce1b5ab75d73488a3b92f5bce7298
93geraldtee <93geraldtee@gmail.com>
2020-03-26 23:50:12 -0400

    add scope methods order by time and title

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  #scope method requirement
+  scope :ordered_by_title, -> { order(title: :asc) }
+  scope :ordered_by_time, -> { order(created_at: :desc) }
+  
 end
